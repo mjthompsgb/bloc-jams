95f9111a73af3b7934af6b23c1cae07b7b46009b
Rob Sturcke <rs@rsvision.com>
2016-02-23 02:33:40 -0500

    landing page errors.2

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,21 +1,21 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var revealPoint = function(point) {
-    points.style.opacity = 1;
-    points.style.transform = "scaleX(1) translateY(0)";
-    points.style.msTransform = "scaleX(1) translateY(0)";
-    points.style.WebkitTransform = "scaleX(1) translateY(0)"; 
- }
+var revealPoint = function (point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)"; 
+};
 
-var animatePoints = function(points) {
+var animatePoints = function (points) {
         forEach(points, revealPoint);     
-    };
+};
 
-window.onload = function() {
+window.onload = function () {
     if (window.innerHeight > 950) {
         animatePoints(pointsArray);
     }
-    
+
     window.addEventListener("scroll", function(event) {
         if (pointsArray[0].getBoundingClientRect().top <= 500) {
             animatePoints(pointsArray);
