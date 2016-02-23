add28df022ddbd9d178ea6f3c92938f605febb51
Rob Sturcke <rs@rsvision.com>
2016-02-23 02:21:09 -0500

    landing page errors

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,15 +1,24 @@
-var animatePoints = function() {
-             var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
+
+var revealPoint = function(point) {
+    points.style.opacity = 1;
+    points.style.transform = "scaleX(1) translateY(0)";
+    points.style.msTransform = "scaleX(1) translateY(0)";
+    points.style.WebkitTransform = "scaleX(1) translateY(0)"; 
+ }
+
+var animatePoints = function(points) {
+        forEach(points, revealPoint);     
+    };
+
+window.onload = function() {
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
     
-             var revealPoint = function(index) {
-                 points[index].style.opacity = 1;
-                 points[index].style.transform = "scaleX(1) translateY(0)";
-                 points[index].style.msTransform = "scaleX(1) translateY(0)";
-                 points[index].style.WebkitTransform = "scaleX(1) translateY(0)"; 
-             }
-             
-             for (var i = 0; i < points.length; i++) {
-                 revealPoint(i);
-             }
-             
-         };
+    window.addEventListener("scroll", function(event) {
+        if (pointsArray[0].getBoundingClientRect().top <= 500) {
+            animatePoints(pointsArray);
+        }    
+    });
+}
